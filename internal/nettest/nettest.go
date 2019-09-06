efdbdc69df0b12578b10d3b8a08b74f6558c2383
David Hill <dhill@mindcry.org>
2019-09-06 14:36:16 -0400

    travis: add go 1.13, tests
    switch to ipv4 for testing.

--- internal/nettest/nettest.go
+++ internal/nettest/nettest.go
@@ -24,14 +24,14 @@ func (s *Server) Close() error {
 	return s.lis.Close()
 }
 
-// NewTLSServer creates a TLS TCP server listening on [::1]:0 and runs
+// NewTLSServer creates a TLS TCP server listening on 127.0.0.1:0 and runs
 // handler with the listener.
 // The listen address with the chosen port can be read from the server's Addr field.
 // Clients should tls.Dial using ClientTLS as the TLS config.
 func NewTLSServer(handler func(lis net.Listener)) *Server {
 	ctx, cancel := context.WithCancel(context.Background())
 	var lc net.ListenConfig
-	lis, err := lc.Listen(ctx, "tcp", "[::1]:0")
+	lis, err := lc.Listen(ctx, "tcp", "127.0.0.1:0")
 	if err != nil {
 		panic(fmt.Sprintf("nettest: NewTLSServer: %v", err))
 	}
